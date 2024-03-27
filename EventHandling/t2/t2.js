3cfc98f2121d9e52d3e38a2bd361ce1c965df8f2
mirohi <m_hintikka@hotmail.com>
2024-03-27 13:10:46 +0200

    add assignment 2

--- EventHandling/t2/t2.js
+++ EventHandling/t2/t2.js
@@ -1,4 +1,6 @@
-const restaurants = [
+'use strict';
+
+let restaurants = [
   {
     location: {type: 'Point', coordinates: [25.018456, 60.228982]},
     _id: '6470d38ecb12107db6fe24c1',
@@ -770,4 +772,86 @@ const restaurants = [
   },
 ];
 
-// your code here
+const table = document.querySelector('table');
+
+const sortedRest = [...restaurants].sort((a, b) =>
+  a.name.toLowerCase().trim().localeCompare(b.name.toLowerCase().trim())
+);
+console.log(sortedRest);
+
+sortedRest.forEach((r) => {
+  const nameTd = document.createElement('td');
+  const addressTd = document.createElement('td');
+  const newRow = document.createElement('tr');
+  const modal = document.querySelector('.modal');
+  const form = document.querySelector('#form');
+  const labels = document.createElement('div');
+  const info = document.createElement('div');
+  const closeButton = document.querySelector('#closeButton');
+  const closeButtonDiv = document.createElement('div');
+
+  closeButton.addEventListener('click', () => {
+    newRow.classList.remove('highlight');
+    modal.close();
+  });
+
+  newRow.addEventListener('click', (event) => {
+    form.innerHTML = '';
+    newRow.classList.toggle('highlight');
+    newRow.style.cursor = 'pointer';
+    const nameDiv = document.createElement('div');
+    const nameLabel = document.createElement('label');
+    nameLabel.textContent = 'Name:';
+    const addressDiv = document.createElement('div');
+    const addressLabel = document.createElement('label');
+    addressLabel.textContent = 'Address:';
+    const postDiv = document.createElement('div');
+    const postLabel = document.createElement('label');
+    postLabel.textContent = 'Zip-code:';
+    const cityDiv = document.createElement('div');
+    const cityLabel = document.createElement('label');
+    cityLabel.textContent = 'City:';
+    const phoneDiv = document.createElement('div');
+    const phoneLabel = document.createElement('label');
+    phoneLabel.textContent = 'Phone:';
+    const companyDiv = document.createElement('div');
+    const companyLabel = document.createElement('label');
+    companyLabel.textContent = 'Company:';
+
+    const nameInfo = document.createElement('p');
+    nameInfo.textContent = r.name;
+    const addressInfo = document.createElement('p');
+    addressInfo.textContent = r.address;
+    const cityInfo = document.createElement('p');
+    cityInfo.textContent = r.city;
+    const phoneInfo = document.createElement('p');
+    phoneInfo.textContent = r.phone;
+    const companyInfo = document.createElement('p');
+    companyInfo.textContent = r.company;
+
+    const contentDiv = document.createElement('div');
+
+    nameDiv.append(nameLabel, nameInfo);
+    nameDiv.classList.add('infoDiv');
+    addressDiv.append(addressLabel, addressInfo);
+    addressDiv.classList.add('infoDiv');
+    cityDiv.append(cityLabel, cityInfo);
+    cityDiv.classList.add('infoDiv');
+    phoneDiv.append(phoneLabel, phoneInfo);
+    phoneDiv.classList.add('infoDiv');
+    companyDiv.append(companyLabel, companyInfo);
+    companyDiv.classList.add('infoDiv');
+
+    contentDiv.append(nameDiv, addressDiv, cityDiv, phoneDiv, companyDiv);
+
+    form.appendChild(contentDiv);
+    modal.showModal();
+  });
+
+  nameTd.textContent = r.name;
+  addressTd.textContent = r.address;
+
+  newRow.appendChild(nameTd);
+  newRow.appendChild(addressTd);
+  table.appendChild(newRow);
+});
